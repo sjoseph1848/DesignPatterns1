c66aab6281c19bebf17603a9242d4477b26465b1
Sam <sjoseph1848@gmail.com>
2017-11-14 23:40:27 -0600

    Add project files.

---
+++ DesignPatterns1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPatterns1
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+    public class Demo
+    {
+
+        static void Main()
+        {
+            var j = new Journal();
+            j.AddEntry("Today was a good day");
+            j.AddEntry("Lakers beat the supersonic");
+            WriteLine(j);
+            ReadKey();
+        }
+    }
+}
+
